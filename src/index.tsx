f30ba80861b185ba82b24bc460800dfec7b2e6d4
roy <roywashington1547@gmail.com>
2024-05-06 23:53:37 -0500

    routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,97 @@
 import { Elysia } from 'elysia';
 import { html } from '@elysiajs/html';
-import { tailwind } from 'elysia-tailwind'; // 1. Import
+import { tailwind } from '@gtramontina.com/elysia-tailwind';
+import { rateLimit } from 'elysia-rate-limit';
+import {
+  BaseHTML,
+  Header,
+  TodoList,
+  RateLimitHtml,
+  validateHTML,
+} from './components';
+import type {
+  Task,
+  NewTaskRequestParams,
+  DeleteTaskRequestParams,
+  UpdateTaskRequestParams,
+} from './misc/types';
+import {
+  initDB,
+  addNewTask,
+  getTasks,
+  deleteTask,
+  updateTask,
+  toggleTask,
+} from './misc/db';
 
-const BaseHTML = ({
-  children,
-}: {
-  children:
-    | JSX.Element
-    | JSX.Element[]
-    | string
-    | number
-    | boolean
-    | null
-    | undefined;
-}) => `
-
-<!DOCTYPE html>
-   <html lang="en">
-  <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width" />
-        <link href="/public/stylesheet.css" rel="stylesheet">
-
-    <title>simple planner</title>
-  </head>
-  <body >
-    <div  id="root">${children}</div>
-  </body>
-</html>
-`;
+initDB();
+let tasks = getTasks();
 
 const app = new Elysia()
   .use(
     tailwind({
-      // 2. Use
-      path: '/public/stylesheet.css', // 2.1 Where to serve the compiled stylesheet;
-      source: './src/styles.css', // 2.2 Specify source file path (where your @tailwind directives are);
-      config: './tailwind.config.js', // 2.3 Specify config file path or Config object;
+      path: '/public/stylesheet.css',
+      source: './src/styles.css',
+      config: './tailwind.config.js',
       options: {
-        // 2.4 Optionally Specify options:
-        minify: true, // 2.4.1 Minify the output stylesheet (default: NODE_ENV === "production");
-        map: true, // 2.4.2 Generate source map (default: NODE_ENV !== "production");
-        autoprefixer: false, // 2.4.3 Whether to use autoprefixer;
+        minify: true,
+        map: true,
+        autoprefixer: false,
       },
     })
   )
   .use(html())
-  .get('/', ({ html }) =>
+  .use(
+    rateLimit({
+      max: 500, // Maximum number of requests allowed per window
+      duration: 3600000, // Duration of rate limit window in milliseconds (1 hour)
+      errorResponse: new Response(validateHTML(RateLimitHtml), {
+        status: 429,
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      }),
+    })
+  )
+  .get('/', ({ html }: { html: any }) =>
     html(
       <BaseHTML>
-        <div class="flex items-center justify-center h-screen">
-          <h1 class="text-violet-600 text-3xl font-bold underline mx-auto text-center">
-            Hello World
-          </h1>
-        </div>
+        <Header />
+        <TodoList tasks={tasks} />
       </BaseHTML>
     )
   )
-  .listen(3000);
+  .post('/newTask', ({ body }: { body: NewTaskRequestParams }) => {
+    tasks = addNewTask(body.task);
 
+    return <TodoList tasks={tasks} />;
+  })
+  .post(
+    '/toggleTask/:id',
+    ({
+      params,
+      query,
+    }: {
+      params: UpdateTaskRequestParams;
+      query: { isDone: String };
+    }) => {
+      tasks = toggleTask(params.id, query.isDone === 'true');
+
+      return <TodoList tasks={tasks} />;
+    }
+  )
+
+  .delete(
+    '/deleteTask/:id',
+    ({ params }: { params: DeleteTaskRequestParams }) => {
+      tasks = deleteTask(params.id);
+
+      return <TodoList tasks={tasks} />;
+    }
+  )
+
+  .get('/tasks', () => tasks)
+  .listen(3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
