3044d3dd12c21319cbea168d7a167c6c0cb0385f
roy <roywashington1547@gmail.com>
2024-05-07 02:12:01 -0500

    styles and tialwind fix

--- src/index.tsx
+++ src/index.tsx
@@ -30,13 +30,15 @@ let tasks = getTasks();
 const app = new Elysia()
   .use(
     tailwind({
-      path: '/public/stylesheet.css',
-      source: './src/styles.css',
-      config: './tailwind.config.js',
+      // 2.0 Use the tailwind plugin;
+      path: '/public/stylesheet.css', // 2.1 Where to serve the compiled stylesheet;
+      source: './src/styles.css', // 2.2 Specify source file path (where your @tailwind directives are);
+      config: './tailwind.config.js', // 2.3 Specify config file path or Config object;
       options: {
-        minify: true,
-        map: true,
-        autoprefixer: false,
+        // 2.4 Optionally Specify options:
+        minify: true, // 2.4.1 Minify the output stylesheet (default: NODE_ENV === "production");
+        map: true, // 2.4.2 Generate source map (default: NODE_ENV !== "production");
+        autoprefixer: false, // 2.4.3 Whether to use autoprefixer;
       },
     })
   )
